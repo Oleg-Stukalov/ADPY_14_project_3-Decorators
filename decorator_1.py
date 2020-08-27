ff3a12e04abb749d5f9b54407c0d309bfeb04c04
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-27 14:11:21 +0300

    draft version on decorator_1.py

--- decorator_1.py
+++ decorator_1.py
@@ -1,31 +1,65 @@
 from datetime import datetime
-
+from collections import OrderedDict
+import json
 
 
 #Написать декоратор - логгер. Он записывает в файл дату и время вызова функции, имя функции, аргументы,
 # с которыми вызвалась и возвращаемое значение.
 
-#Написать декоратор из п.1, но с параметром – путь к логам.
+def decor_logger(param=None):
+    LOG = OrderedDict()
+    LOGFILE = 'log.json'
 
-print(datetime.now().date())
-print(datetime.now().time())
-#имя функции,
+    def _decor_logger(old_function):
+        def new_function(*args, **kwargs):
+            print('Количество лог записей: ', len(LOG))
+            date = str(datetime.now().date())
+            time = str(datetime.now().time())
+            key = (f'Date: {date}, Time: {time}, Function name: {old_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};')
+            print('Key:', key)
+            result = LOG.get(key)
+            # сохранение результата в файл
+            with open(LOGFILE, 'w', encoding='utf-8') as f:
+                json.dump(key, f, ensure_ascii=False, indent=4)
+            print('Список стран со ссылками Википедии успешно сохранен в локальный файл:', LOGFILE)
+            #print(LOG)
+            print(LOGFILE)
+            #return result
+        return new_function
+    return _decor_logger
 
-# аргументы,
-
-# возвращаемое значение
 
 
-# #Проброс параметров
-# def x70(old_function):
-#     def new_function(*args, **kwargs ):
-#         something = old_function(*args, **kwargs
-#         )
-#         something = something * 70
-#         return something
-#     return new_function
+
+@decor_logger()
+def concat(str_1, str_2):
+    return f'{str_1}{str_2}'
+print(concat('abc', 'erd'), '\n')
+print(concat('abc', 'erd1'), '\n')
+
+
+
+
+
+# # #кэширование ф-ий
+# def cachable(param):
+#     CACHE = OrderedDict()
 #
-# @x70
-# def foo(*args, **kwargs ):
-#     ...
-#     return something
+#     def _cachable(old_function):
+#
+#         def new_function(*args, **kwargs):
+#             print(len(CACHE))
+#             key = (old_function.__name__, str(args), str(kwargs))
+#             result = CACHE.get(key)
+#             if result is not None:
+#                 return result
+#             else:
+#                 result = old_function(*args, **kwargs)
+#                 CACHE[key] = result
+#                 if len(CACHE) > param:
+#                     CACHE.popitem(last=False)
+#                 return result
+#
+#         return new_function
+#
+#     return _cachable
