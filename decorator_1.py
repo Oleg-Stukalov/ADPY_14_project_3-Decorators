b9d4f82768dfefb916830c025df02bc83c5bf234
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-28 15:14:28 +0300

    open empty JSON added with ERRORS

--- decorator_1.py
+++ decorator_1.py
@@ -7,29 +7,39 @@ import json
 # с которыми вызвалась и возвращаемое значение.
 
 def decor_logger(param=None):
-    LOG = OrderedDict()
+    #LOG = OrderedDict()
     LOGFILE = 'log.json'
+    log_entry = None
+
+    with open(LOGFILE, 'w', encoding='utf-8') as f:
+        json.dump([], f)
 
     def _decor_logger(old_function):
         def new_function(*args, **kwargs):
-            print('Количество лог записей: ', len(LOG))
+            #print('Количество лог записей: ', len(LOG))
             date = str(datetime.now().date())
             time = str(datetime.now().time())
-            key = (f'Date: {date}, Time: {time}, Function name: {old_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};')
-            print('Key:', key)
-            result = LOG.get(key)
+            log_entry = f'Date: {date}, Time: {time}, Function name: {old_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};'
+            print('***', type(log_entry))
+            print('В лог будет сохранена следующая запись: ', log_entry)
+            #result = LOG.get(key)
             # сохранение результата в файл
             with open(LOGFILE, 'w', encoding='utf-8') as f:
-                json.dump(key, f, ensure_ascii=False, indent=4)
-            print('Список стран со ссылками Википедии успешно сохранен в локальный файл:', LOGFILE)
+                json.dump(log_entry, f, ensure_ascii=False, indent=4)
+            print('Лог-запись успешно добавлена в локальный файл:', LOGFILE)
             #print(LOG)
-            print(LOGFILE)
             #return result
         return new_function
     return _decor_logger
 
 
 
+
+@decor_logger()
+def concat(str_1, str_2):
+    return f'{str_1}{str_2}'
+print(concat('abc', 'erd'), '\n')
+print(concat('abc', 'erd1'), '\n')\
 
 @decor_logger()
 def concat(str_1, str_2):
