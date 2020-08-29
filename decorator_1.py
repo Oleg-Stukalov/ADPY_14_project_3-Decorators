9900bc26b20994ba1dd2abcc41e7e7fed45dc2d3
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-29 12:07:46 +0300

    draft decorator with path added

--- decorator_1.py
+++ decorator_1.py
@@ -1,13 +1,8 @@
 from datetime import datetime
-from collections import OrderedDict
 import json
 
 
-#Написать декоратор - логгер. Он записывает в файл дату и время вызова функции, имя функции, аргументы,
-# с которыми вызвалась и возвращаемое значение.
-
 def decor_logger(param=None):
-    #LOG = OrderedDict()
     LOGFILE = 'log.json'
     log_entry = None
 
@@ -16,24 +11,18 @@ def decor_logger(param=None):
 
     def _decor_logger(old_function):
         def new_function(*args, **kwargs):
-            #print('Количество лог записей: ', len(LOG))
             date = str(datetime.now().date())
             time = str(datetime.now().time())
             log_entry = f'Date: {date}, Time: {time}, Function name: {old_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};'
             print('***', type(log_entry))
             print('В лог будет сохранена следующая запись: ', log_entry)
-            #result = LOG.get(key)
             # сохранение результата в файл
             with open(LOGFILE, 'a', encoding='utf-8') as f:
                 json.dump(log_entry, f, ensure_ascii=False, indent=4)
                 f.write('\n')
             print('Лог-запись успешно добавлена в локальный файл:', LOGFILE)
-            #print(LOG)
-            #return result
         return new_function
     return _decor_logger
-
-
 
 
 @decor_logger()
@@ -51,25 +40,3 @@ print(concat('abc', 'erd4'), '\n')
 
 
 
-# # #кэширование ф-ий
-# def cachable(param):
-#     CACHE = OrderedDict()
-#
-#     def _cachable(old_function):
-#
-#         def new_function(*args, **kwargs):
-#             print(len(CACHE))
-#             key = (old_function.__name__, str(args), str(kwargs))
-#             result = CACHE.get(key)
-#             if result is not None:
-#                 return result
-#             else:
-#                 result = old_function(*args, **kwargs)
-#                 CACHE[key] = result
-#                 if len(CACHE) > param:
-#                     CACHE.popitem(last=False)
-#                 return result
-#
-#         return new_function
-#
-#     return _cachable
---
+++ decorator_2.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import json
+
+
+def decor_logger(param=None):
+    LOGFILE = 'log_path.json'
+    log_entry = None
+
+    with open(LOGFILE, 'w', encoding='utf-8') as f:
+        json.dump([], f)
+
+    def _decor_logger(old_function):
+        def new_function(*args, **kwargs):
+            date = str(datetime.now().date())
+            time = str(datetime.now().time())
+            log_entry = f'Date: {date}, Time: {time}, Function name: {old_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};'
+            print('***', type(log_entry))
+            print('В лог будет сохранена следующая запись: ', log_entry)
+            # сохранение результата в файл
+            with open(LOGFILE, 'a', encoding='utf-8') as f:
+                json.dump(log_entry, f, ensure_ascii=False, indent=4)
+                f.write('\n')
+            print('Лог-запись успешно добавлена в локальный файл:', LOGFILE)
+        return new_function
+    return _decor_logger
+
+
+@decor_logger()
+def concat(str_1, str_2):
+    return f'{str_1}{str_2}'
+print(concat('abc', 'erd'), '\n')
+print(concat('abc', 'erd1'), '\n')
+print(concat('abc', 'erd2'), '\n')
+print(concat('abc', 'erd3'), '\n')
+print(concat('abc', 'erd4'), '\n')
+
+
+
+
+
+
+
