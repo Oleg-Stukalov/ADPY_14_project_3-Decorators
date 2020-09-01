7bf2dca61cfc434658a701daaef46cf033742d10
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-09-01 10:34:54 +0300

    minor changes with ERRORS

--- decorator_1.py
+++ decorator_1.py
@@ -4,7 +4,7 @@ import json
 
 def decor_logger(param=None):
     LOGFILE = 'log.json'
-    log_entry = None
+    #log_entry = None
 
     with open(LOGFILE, 'w', encoding='utf-8') as f:
         json.dump([], f)
@@ -13,13 +13,14 @@ def decor_logger(param=None):
         date = str(datetime.now().date())
         time = str(datetime.now().time())
         log_entry = f'Date: {date}, Time: {time}, Function name: {new_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};'
-        print('***', type(log_entry))
+        #print('***', type(log_entry))
         print('В лог будет сохранена следующая запись: ', log_entry)
         # сохранение результата в файл
         with open(LOGFILE, 'a', encoding='utf-8') as f:
             json.dump(log_entry, f, ensure_ascii=False, indent=4)
             f.write('\n')
         print('Лог-запись успешно добавлена в локальный файл:', LOGFILE)
+        return log_entry
     return new_function
 
 
--- decorator_2.py
+++ decorator_2.py
@@ -23,20 +23,23 @@ def decor_logger(path):
         date = str(datetime.now().date())
         time = str(datetime.now().time())
         log_entry = f'Date: {date}, Time: {time}, Function name: {new_function.__name__}, Args: {str(args)}, Kwargs: {str(kwargs)};'
-        print('***', type(log_entry))
+        #print('***', type(log_entry))
         print('В лог будет сохранена следующая запись: ', log_entry)
         # сохранение результата в файл
         with open(LOGFILE, 'a', encoding='utf-8') as f:
             json.dump(log_entry, f, ensure_ascii=False, indent=4)
             f.write('\n')
         print('Лог-запись успешно добавлена в локальный файл:', LOGFILE)
+        #print('***', new_function.__name__)
+        return log_entry
     return new_function
+    #return new_function.__name__
 #return decor_logger
 
 @decor_logger(LOG_PATH)
 def concat(str_1, str_2):
     return print(f'{str_1}{str_2}')
-print(concat('abc', 'erd'))
+print(concat('abc', 'erd'), '\n')
 print(concat('abc', 'erd1'), '\n')
 print(concat('abc', 'erd2'), '\n')
 print(concat('abc', 'erd3'), '\n')
